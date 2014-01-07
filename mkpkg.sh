30f86443c28ba7e03f982cc33731ffce1fb6fc7d
opalenzuela <opalenzuela@gmail.com>
2014-01-07 14:02:11 +0100

    fixing mkpgk

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-tar cvfz $odcgi-`date '+%Y%m%d'`.od.noarch.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
+PKGID="odcgi"
+tar cvfz $PKGID-`date '+%Y%m%d'`.od.noarch.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
