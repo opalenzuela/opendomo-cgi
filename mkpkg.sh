c59e49a7ee9621852f3b2e5de0f0695ceb6bb5a9
opalenzuela <opalenzuela@gmail.com>
2014-01-08 13:44:14 +0100

    package filename modified

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 PKGID="odcgi"
-tar cvfz $PKGID-`date '+%Y%m%d'`.od.noarch.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
+tar cvfz $PKGID-`date '+%Y%m%d'`.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
