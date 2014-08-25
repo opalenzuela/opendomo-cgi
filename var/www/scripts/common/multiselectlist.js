94267d3f47af90143161f7a1533a1e6935b4c3b3
opalenzuela <opalenzuela@gmail.com>
2014-08-25 17:12:31 +0200

    merging with standard theme

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -1,7 +1,7 @@
 $(function() {
 	var f = $("fieldset")[0];
-	if (f.hasClass("selectable")  ) {
-		f.find("li").on("click", function(item) {
+	if ($(f).hasClass("selectable")  ) {
+		$(f).find("li").on("click", function(item) {
 			console.log("Clicked on " , $(this));
 		});
 	}
