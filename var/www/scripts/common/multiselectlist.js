0acb91c21e75da3db01b4f11d658439736914d38
opalenzuela <opalenzuela@gmail.com>
2014-08-26 10:18:58 +0200

    several corrections

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -3,19 +3,33 @@ include_script("/scripts/jquery.longpress.js");
 var timetouch;
 $(function() {
 	$("fieldset.selectable").each(function(index){
-		var url = $(this).find("a").href ;
-		$(this).find("a").href ="";
+		var url = $(this).find("a").attr("href") ;
+		$(this).find("a").attr("href","javascript:void()");
 		$(this).find("li").on("mousedown touchstart", function(event) {
 			timetouch = new Date().getTime();
 			console.log("Pressing on " , $(this));
 		});
 		$(this).find("li").on("mouseup touchend", function(event) {
-			var timetouch2 = new Date().getTime();
-			var seconds = timetouch2 - timetouch;
-			timetouch = 0;
-			console.log("Pressed for " + seconds + " on " , $(this));
 			event.preventDefault();
 			event.stopPropagation();			
+			
+			var timetouch2 = new Date().getTime();
+			var milliseconds = timetouch2 - timetouch;
+			timetouch = 0;
+			console.log("Pressed for " + milliseconds + " on " , $(this));
+			if (milliseconds<1000) {
+				window.location = url;
+			} else {
+				if ($(this).find("input")[0].prop('checked')) {
+					$(this).find("input")[0].checked = false;
+					$(this).remvoveClass("selected");
+				} else {
+					$(this).addClass("selected");
+					$(this).find("input")[0].checked = true;
+				}
+			}
+			
+
 		});		
 		
 	});
