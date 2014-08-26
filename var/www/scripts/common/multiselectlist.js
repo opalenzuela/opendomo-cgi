9200c4541fb7ec1c120ac19a0b5badd4e247c479
opalenzuela <opalenzuela@gmail.com>
2014-08-26 10:32:25 +0200

    trying with a timed function

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -1,15 +1,19 @@
 include_script("/scripts/jquery.longpress.js");
 
 var timetouch;
+var focusitem = null;
 $(function() {
 	$("fieldset.selectable").each(function(index){
 		var url = $(this).find("a").attr("href") ;
-		$(this).find("a").attr("href","javascript:void()");
+		$(this).find("a")
+			.attr("href","javascript:void()")
+			.data("target",url);
 		$(this).find("li").on("mousedown touchstart", function(event) {
+			focusitem = $(this);
 			timetouch = new Date().getTime();
 			console.log("Pressing on " , $(this));
 		});
-		$(this).find("li").on("mouseup touchend", function(event) {
+		/*$(this).find("li").on("mouseup touchend", function(event) {
 			event.preventDefault();
 			event.stopPropagation();			
 			
@@ -20,31 +24,31 @@ $(function() {
 			if (milliseconds<1000) {
 				window.location = url;
 			} else {
-				if ($(this).find("input")[0].prop('checked')) {
+				if ($(this).find("input")[0].checked) {
 					$(this).find("input")[0].checked = false;
-					$(this).remvoveClass("selected");
+					$(this).removeClass("selected");
 				} else {
-					$(this).addClass("selected");
 					$(this).find("input")[0].checked = true;
+					$(this).addClass("selected");
 				}
 			}
-			
-
-		});		
-		
+		});	*/	
 	});
-	/*$('fieldset.selectable li').longpress(function(event) {
-		// longpress callback
-		if ($(this).hasClass("selected")){
-			$(this).removeClass("selected");
-			$(this).find("input").attr("checked","");
-		} else {
-			$(this).addClass("selected");
-			$(this).find("input").attr("checked","checked");
-		}
-		console.log('You just longpress-ed an item.', event); 
-		event.preventDefault();
-		event.stopPropagation();
-	});*/
 });
+
+setTimeout(updateLongpress,100);
+
+function updateLongpress() {
+	if (focusitem == null) return false;
+	var timetouch2 = new Date().getTime();
+	var milliseconds = timetouch2 - timetouch;
+	if (milliseconds>1000) {
+		if (focusitem.hasClass("selected")){
+			focusitem.removeClass("selected");	
+		} else {
+			focusitem.addClass("selected");
+		}
+		focusitem = null;
+	}
+}
 
