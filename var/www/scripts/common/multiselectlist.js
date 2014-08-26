9cf5a23a9ef71b4f40c739cb5cda7f6c66c97acf
opalenzuela <opalenzuela@gmail.com>
2014-08-26 09:24:41 +0200

    Longtouch test

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -1,9 +1,22 @@
 $(function() {
 	var f = $("fieldset")[0];
 	if ($(f).hasClass("selectable")  ) {
-		$(f).find("li").on("click", function(item) {
+		$(f).find("li").on("click", function(event) {
 			console.log("Clicked on " , $(this));
 		});
 	}
 });
 include_script("/scripts/jquery.longpress.js");
+$('fieldset.selectable li').longpress(function(event) {
+    // longpress callback
+	if ($(this).hasClass("selected")){
+		$(this).removeClass("selected");
+		$(this).find("input").attr("checked","");
+	} else {
+		$(this).addClass("selected");
+		$(this).find("input").attr("checked","checked");
+	}
+    console.log('You just longpress-ed an item.', event); 
+	event.preventDefault();
+	event.stopPropagation();
+});
