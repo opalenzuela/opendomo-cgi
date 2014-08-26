bc8c76d7eb9cc2f0ec28a6b0bb134a5bab7f296a
opalenzuela <opalenzuela@gmail.com>
2014-08-26 13:01:57 +0200

    testing filter list

--- var/www/scripts/common/filterablelist.js
+++ var/www/scripts/common/filterablelist.js
@@ -1,5 +1,29 @@
 $(function() {
-	$("fieldset.filterable").each(function(item) {
-		$(this).prepend("<input name='filter' type='search'>");
+	$("fieldset.filterable ul").each(function(item) {
+		var rawhtml = "<input name='filter' placeholder='Filter text' class='filterbox' data-reference='" + $(this).attr('id') + "'  id='" + $(this).attr('id') + "_filter' type='search'>";
+		$(rawhtml).insertBefore($(this));
+	});
+	$("fieldset.filterable input.filterbox").on("change",function() {
+		var ref = $(this).data("reference");
+		filterList(ref, $(this).val());
 	});
 });
+
+/**
+ * Filter the contents of a list
+ * 
+ * This function hides all the items in a list that doesn't match the specified string
+ * 
+ * @author	Oriol Palenzuela
+ * @param listID	list's ID
+ * @param text 		string to match the selection
+ */
+function filterList(listID,text){
+	$("#" + listID+ " li").each(function(item){
+		if ($(this).text().indexOf(text)==-1){ 
+			$(this).addClass("hidden");
+		} else {
+			$(this).removeClass("hidden");
+		}
+	});
+}
