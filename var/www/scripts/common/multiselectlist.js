4c4fa338b5007a5608d86df79b0cffc526499688
opalenzuela <opalenzuela@gmail.com>
2014-08-26 10:06:07 +0200

    itereating for multiple fieldsets

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -2,13 +2,14 @@ include_script("/scripts/jquery.longpress.js");
 
 var timetouch;
 $(function() {
-	var f = $("fieldset")[0];
-	if ($(f).hasClass("selectable")  ) {
-		$(f).find("li").on("mousedown touchstart", function(event) {
+	$("fieldset.selectable").each(function(item){
+		var url = $(item).find("a").href ;
+		$(item).find("a").href ="";
+		$(item).find("li").on("mousedown touchstart", function(event) {
 			timetouch = new Date().getTime();
 			console.log("Pressing on " , $(this));
 		});
-		$(f).find("li").on("mouseup touchend", function(event) {
+		$(item).find("li").on("mouseup touchend", function(event) {
 			var timetouch2 = new Date().getTime();
 			var seconds = timetouch2 - timetouch;
 			timetouch = 0;
@@ -16,7 +17,8 @@ $(function() {
 			event.preventDefault();
 			event.stopPropagation();			
 		});		
-	}
+		
+	});
 	/*$('fieldset.selectable li').longpress(function(event) {
 		// longpress callback
 		if ($(this).hasClass("selected")){
