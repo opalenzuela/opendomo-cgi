488f09fb120124e9a5ae7ec608a7a36837cc3f44
opalenzuela <opalenzuela@gmail.com>
2014-08-26 10:09:54 +0200

    wrong use of each()

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -2,14 +2,14 @@ include_script("/scripts/jquery.longpress.js");
 
 var timetouch;
 $(function() {
-	$("fieldset.selectable").each(function(item){
-		var url = $(item).find("a").href ;
-		$(item).find("a").href ="";
-		$(item).find("li").on("mousedown touchstart", function(event) {
+	$("fieldset.selectable").each(function(index){
+		var url = $(this).find("a").href ;
+		$(this).find("a").href ="";
+		$(this).find("li").on("mousedown touchstart", function(event) {
 			timetouch = new Date().getTime();
 			console.log("Pressing on " , $(this));
 		});
-		$(item).find("li").on("mouseup touchend", function(event) {
+		$(this).find("li").on("mouseup touchend", function(event) {
 			var timetouch2 = new Date().getTime();
 			var seconds = timetouch2 - timetouch;
 			timetouch = 0;
