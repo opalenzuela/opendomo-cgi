041076a34f01794860fe0b4d19ce68d41473864b
opalenzuela <opalenzuela@gmail.com>
2014-08-26 09:49:08 +0200

    trying with native methods

--- var/www/scripts/common/multiselectlist.js
+++ var/www/scripts/common/multiselectlist.js
@@ -1,13 +1,23 @@
-include_script("/scripts/jquery.longpress.js");
+//include_script("/scripts/jquery.longpress.js");
 
+var timetouch;
 $(function() {
 	var f = $("fieldset")[0];
 	if ($(f).hasClass("selectable")  ) {
-		$(f).find("li").on("click", function(event) {
-			console.log("Clicked on " , $(this));
+		$(f).find("li").on("mousedown touchstart", function(event) {
+			timetouch = new Date().getTime();
+			console.log("Pressing on " , $(this));
 		});
+		$(f).find("li").on("mouseup touchend", function(event) {
+			var timetouch2 = new Date().getTime();
+			var seconds = timetouch2 - timetouch;
+			timetouch = 0;
+			console.log("Pressed for " + seconds + " on " , $(this));
+			event.preventDefault();
+			event.stopPropagation();			
+		});		
 	}
-	$('fieldset.selectable li').longpress(function(event) {
+	/*$('fieldset.selectable li').longpress(function(event) {
 		// longpress callback
 		if ($(this).hasClass("selected")){
 			$(this).removeClass("selected");
@@ -19,6 +29,6 @@ $(function() {
 		console.log('You just longpress-ed an item.', event); 
 		event.preventDefault();
 		event.stopPropagation();
-	});
+	});*/
 });
 
