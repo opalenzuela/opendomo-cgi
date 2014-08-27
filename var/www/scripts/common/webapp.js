47003b26d0783486f0bf670c3e2ed7a4a6d236e2
opalenzuela <opalenzuela@gmail.com>
2014-08-27 17:14:35 +0200

    Testing webapp mode

--- var/www/scripts/common/webapp.js
+++ var/www/scripts/common/webapp.js
@@ -1,18 +1,25 @@
-//$(function(){
-//if(("standalone" in window.navigator) && window.navigator.standalone){
-	var noddy, remotes = false;
-	document.addEventListener('click touch', function(event) {
-		console.log("Handeling click event", event);
-		noddy = event.target;
-		while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-			noddy = noddy.parentNode;
-		}
+$(function(){
+	if(("standalone" in window.navigator) && window.navigator.standalone){
+		jQuery("a").on("click",function(e){
+			console.log("Clicked",e);
+			var href = $(this).attr("href");
+			if (href) document.location.href = href
+			e.preventDefault();
+		})
+		/*
+		var noddy, remotes = false;
+		document.addEventListener('click touch', function(event) {
+			console.log("Handeling click event", event);
+			noddy = event.target;
+			while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
+				noddy = noddy.parentNode;
+			}
 
-		if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
-		{
-			event.preventDefault();
-			document.location.href = noddy.href;
-		}
-	},false);
-//}
-//});
+			if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
+			{
+				event.preventDefault();
+				document.location.href = noddy.href;
+			}
+		},false); */
+	} 
+});
