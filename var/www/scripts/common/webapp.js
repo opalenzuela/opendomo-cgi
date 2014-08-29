57860306083c14f7e9a096437a66c155685c7e01
Oriol Palenzuela <opalenzuela@gmail.com>
2014-08-29 17:12:10 +0200

    Update webapp.js

--- var/www/scripts/common/webapp.js
+++ var/www/scripts/common/webapp.js
@@ -5,21 +5,6 @@ $(function(){
 			var href = $(this).attr("href");
 			if (href) document.location.href = href
 			e.preventDefault();
-		})
-		/*
-		var noddy, remotes = false;
-		document.addEventListener('click touch', function(event) {
-			console.log("Handeling click event", event);
-			noddy = event.target;
-			while(noddy.nodeName !== "A" && noddy.nodeName !== "HTML") {
-				noddy = noddy.parentNode;
-			}
-
-			if('href' in noddy && noddy.href.indexOf('http') !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes))
-			{
-				event.preventDefault();
-				document.location.href = noddy.href;
-			}
-		},false); */
+		});
 	} 
 });
