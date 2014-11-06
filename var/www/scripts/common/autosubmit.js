3fe1c3b590eb679e2c618073e9542e35efe8f0d2
opalenzuela <opalenzuela@gmail.com>
2014-11-06 11:03:09 +0100

    Preparing auto-submit forms

---
+++ var/www/scripts/common/autosubmit.js
@@ -0,0 +1,33 @@
+$(function(){
+	$("fieldset.autosubmit input").on("change",function(){
+		var id = $(this).closest("form").attr("id");
+		submitForm(id);
+	});
+});
+
+function submitForm(formID) {
+    $("#"+formID).submit(function(e){
+		e.preventDefault();
+ 
+        var dataString = $("#"+formID).serialize();
+     
+        $.ajax({
+        type: "POST",
+        url: $("#"+formID).attr("action")+"?GUI=XML",
+		async: false; 
+        data: dataString,
+        dataType: "text/xml",
+        success: function(data) {
+			console.log("Success");
+			console.log(data);
+			},
+		fail: function(data) {
+			console.log("Fail");
+			console.log(data);
+			}
+           
+        });         
+     
+	});
+
+}
