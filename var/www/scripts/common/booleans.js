c889e6ede5a2f507aa8f36e2f3bdc66475d282f5
opalenzuela <opalenzuela@gmail.com>
2014-11-06 10:23:49 +0100

    Enabling visuals for booleans

--- var/www/scripts/common/booleans.js
+++ var/www/scripts/common/booleans.js
@@ -3,8 +3,12 @@ $(function(){
 		var c = $(this).find("input").prop("checked");
 		if (c!=true) {
 			$(this).addClass("checked");
+			$(this).removeClass("unchecked");
 		} else {
+			$(this).addClass("unchecked");
 			$(this).removeClass("checked");
 		}
 	});
+	// Refresh all booleans
+	$("li.boolean").trigger("click");
 });
