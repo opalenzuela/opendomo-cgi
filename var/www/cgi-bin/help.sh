82ca79ac0cb1dc117ba711bec834f2ac031a4d5e
opalenzuela <opalenzuela@gmail.com>
2014-12-01 11:53:20 +0100

    Adjusting parameters

--- var/www/cgi-bin/help.sh
+++ var/www/cgi-bin/help.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 echo "Content-type: text/html"
 echo
-TOPIC="$1"
+TOPIC="echo $QUERY_STRING | cut -f1 -d."
 echo "<html><head><link rel='stylesheet' type='text/css' media='screen' href='/cgi-bin/css.cgi?admin'></head><body>"
 if test -f /usr/local/opendomo/docs/$TOPIC.txt; then
+	echo "<fieldset>"
 	cat /usr/local/opendomo/docs/$TOPIC | sed 's/^/# /'
+	echo "</fieldset>"
 else
-	echo "<div id='contents'>Manual page not found</div>"
+	echo "<fieldset>"
+	echo "<legend>$TOPIC</legend>"
+	i18n.sh "Manual page not found"
+	echo "</fieldset>"
 fi
-echo "<div id='moreinfo'><a href='http://www.opendomo.com/wiki/index.php?title=$TOPIC' target='_new'>More information</a></div>"
+echo "<div class='toolbar'><a href='http://www.opendomo.com/wiki/index.php?title=$TOPIC' target='_new'>"
+i18n.sh "More information"
+echo "</a></div>"
 echo "</body></html>"
